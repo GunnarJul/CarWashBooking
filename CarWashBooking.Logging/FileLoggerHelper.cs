838ad9109ec9a7a65573fcffb45a5e6391e55ea9
Gunnar Jul Jensen <gjj@envidan.dk>
2019-06-07 07:43:34 +0200

    logging

---
+++ CarWashBooking.Logging/FileLoggerHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+
+namespace CarWashBooking.Logging
+{
+    internal class FileLoggerHelper
+    {
+        private string fileName;
+
+        public FileLoggerHelper(string fileName)
+        {
+            this.fileName = fileName;
+        }
+
+        static ReaderWriterLock locker = new ReaderWriterLock();
+
+        internal void InsertLog(LogEntry logEntry)
+        {
+            var directory = System.IO.Path.GetDirectoryName(fileName);
+
+            if (!System.IO.Directory.Exists(directory))
+                System.IO.Directory.CreateDirectory(directory);
+
+            try
+            {
+                locker.AcquireWriterLock(int.MaxValue);
+                System.IO.File.AppendAllText(fileName, $"{logEntry.CreatedTime} {logEntry.EventId} {logEntry.LogLevel} {logEntry.Message}" + Environment.NewLine);
+            }
+            finally
+            {
+                locker.ReleaseWriterLock();
+            }
+        }
+
+    }
+}
